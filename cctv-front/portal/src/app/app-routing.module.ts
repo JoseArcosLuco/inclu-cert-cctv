efc8dc3c618a18ade96c58466a8ef1757b6402a6
jose.arcos.luco <jose.arcos.luco@gmail.com>
2024-07-02 01:21:22 -0400

    test de componentes

--- cctv-front/portal/src/app/app-routing.module.ts
+++ cctv-front/portal/src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+
 
 const routes: Routes = [
   {path:'', redirectTo:'login',pathMatch:'full'},
   {path:'login', component:LoginComponent},
-  {path:'home', component:PagesComponent},
-  {path:'dashboard', component:DashboardComponent},
+  
   {path:'**', component: LoginComponent},
 ];
 
--- cctv-front/portal/src/app/app.module.ts
+++ cctv-front/portal/src/app/app.module.ts
@@ -1,25 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-
-
+import { MainSidebarComponent } from './shared/main-sidebar/main-sidebar.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainSidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    RouterModule
   ],
   providers: [
     provideClientHydration()
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
