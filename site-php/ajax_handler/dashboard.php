0cdbada0104aa3c25bf2287bdbfa09d49527c86c
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-09-05 16:44:07 -0400

    Se agrega contenido dínamico a las cards del dashboard según los parámetros ingresados. Quizás el código agregado se necesite refactorizar.

--- site-php/ajax_handler/dashboard.php
+++ site-php/ajax_handler/dashboard.php
@@ -1,5 +1,7 @@
 <?php
 require_once('../includes/Charts.class.php');
+require_once('../includes/Users.class.php');
+require_once('../includes/Plantas.class.php');
 header('Content-Type: application/json');
 
 if (isset($_POST)) {
@@ -11,29 +13,48 @@ if (isset($_POST)) {
             $response = ChartData::obtenerChartData();
             echo json_encode($response);
             break;
+        
+        case 'updateCardsWithoutCliente':
+            $id_cliente = '';
+            $id_planta = '';
+            $response['countUsers'] = Users::countUsers();
+            $response['countPlantas'] = Plantas::countPlantas($id_cliente);
+            $response['countReportes'] = ChartData::countReportesPlantas($id_planta, '', '');
+            $response['porcentaje'] = ChartData::porcentajeReportes($id_cliente, $id_planta);
+            echo json_encode($response);
+            break;
 
         case 'updateChartClientes':
-            $id = $_POST['id'];
-            $response = ChartData::obtenerDatosClientes($id);
+            $id_planta = '';
+            $id_cliente = $_POST['id'];
+            $response = ChartData::obtenerDatosClientes($id_cliente);
+            $response['countPlantas'] = Plantas::countPlantas($id_cliente);
+            $response['porcentaje'] = ChartData::porcentajeReportes($id_cliente, $id_planta);
             echo json_encode($response);
             break;
 
         case 'updateChartWithoutFecha':
 
+            $id_cliente = '';
             $id_planta = $_POST['id_planta'];
 
             $chart_data = ChartData::obtenerDatosWithoutFecha($id_planta);
+            $chart_data['countReportes'] = ChartData::countReportesPlantas($id_planta, '', '');
+            $chart_data['porcentaje'] = ChartData::porcentajeReportes($id_cliente, $id_planta);
 
             echo json_encode($chart_data);
             break;
 
         case 'updateChartWithFecha':
 
+            $id_cliente = '';
             $id_planta = $_POST['id_planta'];
             $fecha_inicio = $_POST['fecha_inicio'];
             $fecha_fin = $_POST['fecha_fin'];
 
             $chart_data = ChartData::obtenerDatosWithFecha($id_planta, $fecha_inicio, $fecha_fin);
+            $chart_data['countReportes'] = ChartData::countReportesPlantas($id_planta, $fecha_inicio, $fecha_fin);
+            $chart_data['porcentaje'] = ChartData::porcentajeReportes($id_cliente, $id_planta, $fecha_inicio, $fecha_fin);
 
             echo json_encode($chart_data);
             break;
